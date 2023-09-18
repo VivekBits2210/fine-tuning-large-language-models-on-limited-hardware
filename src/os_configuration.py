f5a88ec8ec0cb5faeb82446f79e690562c53ee66
VivekBits2210 <viveknayak2210@gmail.com>
2023-09-18 18:18:25 -0400

    OOP Framework part 1

---
+++ src/os_configuration.py
@@ -0,0 +1,32 @@
+import os
+from typing import Dict, Any
+
+
+class OSConfiguration:
+    def __init__(self) -> None:
+        self.env = dict(os.environ)
+
+    def get_var(self, key) -> str:
+        return self.env.get(key)
+
+    def set_var_lazy(self, key: str, value: Any) -> None:
+        self.env[key] = str(value)
+
+    def delete_var_lazy(self, key: str) -> None:
+        if key in self.env:
+            del self.env[key]
+
+    def commit_changes(self) -> None:
+        for key, value in self.env.items():
+            os.environ[key] = value
+
+    def update_from_dict(self, mapping: Dict[str, Any]) -> None:
+        for key, value in mapping.items():
+            self.set_var_lazy(key, value)
+        self.commit_changes()
+
+    def __repr__(self):
+        return "\n".join([f"{key}={value}" for key, value in self.env.items()])
+
+    def __str__(self):
+        return self.__repr__()
---
+++ src/packaging_utils.py
@@ -0,0 +1,21 @@
+from typing import List
+import sys
+
+
+class PackagePathManager:
+    INTEL_PATH = "/share/apps/python/3.8.6/intel/lib/python3.8/site-packages"
+
+    def __init__(self, net_id: str):
+        self.net_id = net_id
+        self.local_path = self._local_path()
+
+    def _local_path(self) -> str:
+        return f"/home/{self.net_id}/.local/lib/python3.8/site-packages"
+
+    def fetch_package_paths(self) -> List[str]:
+        return [self.local_path, self.INTEL_PATH]
+
+    def add_package_paths_to_system(self) -> None:
+        paths = self.fetch_package_paths()
+        for index, path in enumerate(paths):
+            sys.path.insert(index, path)
