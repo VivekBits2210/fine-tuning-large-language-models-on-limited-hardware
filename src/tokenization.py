7917c58ddfe395f09ea20e523a1b12e7615283dd
VivekBits2210 <viveknayak2210@gmail.com>
2023-09-19 00:58:26 -0400

    Add methods for data collation

--- src/tokenization.py
+++ src/tokenization.py
@@ -1,15 +1,27 @@
 from transformers import AutoTokenizer
+from transformers import DataCollatorForLanguageModeling
 
 
 class TokenizerFactory:
     def __init__(self, save_path: str) -> None:
         self.save_path = save_path
+        self.tokenizer = None
+        self.data_collator = None
 
-    def load_tokenizer(self, load_path: str):
-        return AutoTokenizer.from_pretrained(load_path)
+    def load_tokenizer_from_path(self, load_path: str) -> None:
+        self.tokenizer = AutoTokenizer.from_pretrained(load_path)
+        return self.tokenizer
 
     def create_tokenizer(self, model_name: str):
-        tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=self.save_path)
-        tokenizer.pad_token = tokenizer.eos_token
-        tokenizer.padding_side = 'left'
-        return tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=self.save_path)
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.tokenizer.padding_side = 'left'
+        return self.tokenizer
+
+    def get_data_collator(self):
+        if self.data_collator:
+            return self.data_collator
+
+        self.data_collator = DataCollatorForLanguageModeling(tokenizer=self.tokenizer, mlm=False)
+        return self.data_collator
+
