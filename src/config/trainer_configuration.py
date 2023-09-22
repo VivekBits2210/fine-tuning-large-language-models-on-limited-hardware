9a524b58f5fec795c94e88a16bec428d6d6c7334
VivekBits2210 <viveknayak2210@gmail.com>
2023-09-21 22:22:04 -0400

    Higher validation interval

--- src/config/trainer_configuration.py
+++ src/config/trainer_configuration.py
@@ -2,5 +2,5 @@ class TrainerConfiguration:
     def __init__(self, **kwargs):
         self.epochs = kwargs.get("epochs", 2)
         self.sampling_interval = kwargs.get("sampling_interval", 50)
-        self.validation_interval = kwargs.get("validation_interval", 5)
+        self.validation_interval = kwargs.get("validation_interval", 500)
         self.checkpointing_interval = kwargs.get("checkpointing_interval", 1000)
